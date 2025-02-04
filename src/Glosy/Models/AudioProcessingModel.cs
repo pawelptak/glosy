209330c489177655bbff8a20a23f1f8ced9b5bb5
pawelptak <97pawelptak@gmail.com>
2025-02-04 19:43:15 +0100

    set character limit for text prompt

--- src/Glosy/Models/AudioProcessingModel.cs
+++ src/Glosy/Models/AudioProcessingModel.cs
@@ -1,10 +1,14 @@
-﻿namespace Glosy.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Glosy.Models
 {
     public class AudioProcessingModel
     {
         public required string ModelName { get; set; }
         public IFormFile SourceFile { get; set; }
         public required IFormFile TargetFile { get; set; }
+
+        [StringLength(224)] // This is the limit for Polish language
         public string TextPrompt { get; set; }
         public required IFormFile OutputFile { get; set; }
     }
