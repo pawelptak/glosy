77e587fdc38685da16a997c5ad8c07c4f0616a8e
pawelptak <97pawelptak@gmail.com>
2025-02-05 21:12:13 +0100

    code redundancy improvements

--- src/Glosy/Services/AudioProcessingService.cs
+++ src/Glosy/Services/AudioProcessingService.cs
@@ -8,7 +8,7 @@ namespace Glosy.Services
     {
         private readonly string _pythonPath;
         private readonly string _ffmpegPath;
-        private readonly string _outputDirectory = @"Multimedia";
+        private readonly string _tempFilesDirectory = @"Multimedia";
 
         public AudioProcessingService(IConfiguration configuration)
         {
@@ -16,79 +16,82 @@ namespace Glosy.Services
             _ffmpegPath = configuration["Config:FFmpegPath"];
         }
 
-        public async Task<string> ConvertVoiceAsync(AudioProcessingModel model)
-        {
-            var scriptPath = @"PythonScripts\conversion.py";
-            model.ModelName = "voice_conversion_models/multilingual/multi-dataset/openvoice_v2";
-
-            var sourceFilePath = Path.Combine(_outputDirectory, Path.GetRandomFileName() + Path.GetExtension(model.SourceFile.FileName));
-            using (var sourceStream = new FileStream(sourceFilePath, FileMode.Create))
-            {
-                await model.SourceFile.CopyToAsync(sourceStream);
-            }
-
-            if (model.SourceFile.ContentType == "audio/webm")
-            {
-                var convertedFileName = "pies.wav";
-                var convertedFilePath = Path.Combine(Path.GetDirectoryName(sourceFilePath), convertedFileName);
-                ConvertWebmToWav(sourceFilePath, convertedFilePath);
-
-                sourceFilePath = convertedFilePath;
-            }
-
-            var targetFilePath = Path.Combine(_outputDirectory, Path.GetRandomFileName() + Path.GetExtension(model.TargetFile.FileName));
-            using (var targetStream = new FileStream(targetFilePath, FileMode.Create))
-            {
-                await model.TargetFile.CopyToAsync(targetStream);
-            }
-
-            var outputFilePath = Path.Combine("generated", "out.wav");
-            var fullOutputFilePath = Path.Combine("wwwroot", outputFilePath);
-            Directory.CreateDirectory(Path.GetDirectoryName(fullOutputFilePath));
-
-            var arguments = $"{model.ModelName} {sourceFilePath} {targetFilePath} {fullOutputFilePath}";
-
-            var result = RunPythonScript(_pythonPath, scriptPath, arguments);
-
-            return outputFilePath;
-        }
-
         public async Task<string> SynthesizeVoiceAsync(AudioProcessingModel model)
         {
             var scriptPath = @"PythonScripts\synthesis.py";
-            model.ModelName = "tts_models/multilingual/multi-dataset/xtts_v2";
+            model.ModelName = "tts_models/multilingual/multi-dataset/xtts_v2"; // TODO: assigning it to the model may not be necessary. I leave it for now.
 
-            var targetFilePath = Path.Combine(_outputDirectory, Path.GetRandomFileName() + Path.GetExtension(model.TargetFile.FileName));
-            using (var targetStream = new FileStream(targetFilePath, FileMode.Create))
-            {
-                await model.TargetFile.CopyToAsync(targetStream);
-            }
-
-            var outputFilePath = Path.Combine("generated", "out.wav");
+            var outputFilePath = Path.Combine("generated", "out.wav"); // to show output file preview in the UI, the file path mustn't have the 'wwwroot' folder
             var fullOutputFilePath = Path.Combine("wwwroot", outputFilePath);
-            Directory.CreateDirectory(Path.GetDirectoryName(fullOutputFilePath));
+
+            var targetFilePath = Path.Combine(_tempFilesDirectory, model.TargetFile.FileName);
 
             var language = "pl";
             var arguments = $"{model.ModelName} \"{model.TextPrompt}\" {targetFilePath} {language} {fullOutputFilePath}";
-
-            var result = RunPythonScript(_pythonPath, scriptPath, arguments);
+            await ProcessVoice(model, scriptPath, arguments, fullOutputFilePath);
 
             return outputFilePath;
         }
 
-        private static string RunPythonScript(string pythonPath, string scriptPath, string arguments)
+        public async Task<string> ConvertVoiceAsync(AudioProcessingModel model)
         {
-            ProcessStartInfo psi = new ProcessStartInfo
+            var scriptPath = @"PythonScripts\conversion.py";
+            model.ModelName = "voice_conversion_models/multilingual/multi-dataset/openvoice_v2"; // TODO: assigning it to the model may not be necessary. I leave it for now.
+            var outputFileName = "out.wav";
+
+
+            var sourceFilePath = Path.Combine(_tempFilesDirectory, model.SourceFile.FileName);
+            await SaveStreamToDrive(sourceFilePath, model.SourceFile);
+
+            if (string.Equals(model.SourceFile.ContentType, "audio/webm")) // Audio recorded using microphone has to be converted to mp4, otherwise it doesn't work idk why
             {
-                FileName = pythonPath,
-                Arguments = $"{scriptPath} {arguments}",
+                var convertedFilePath = Path.Combine(Path.GetDirectoryName(sourceFilePath), outputFileName);
+                await ConvertToWavAsync(sourceFilePath, convertedFilePath);
+                sourceFilePath = convertedFilePath;
+            }
+
+            var targetFilePath = Path.Combine(_tempFilesDirectory, model.TargetFile.FileName);
+
+            var outputFilePath = Path.Combine("generated", "out.wav"); // to show output file preview in the UI, the file path mustn't have the 'wwwroot' folder
+            var fullOutputFilePath = Path.Combine("wwwroot", outputFilePath);
+
+            var arguments = $"{model.ModelName} {sourceFilePath} {targetFilePath} {fullOutputFilePath}";
+            await ProcessVoice(model, scriptPath, arguments, fullOutputFilePath);
+
+            return outputFilePath;
+        }
+
+        private async Task<string> ProcessVoice(AudioProcessingModel model, string scriptPath, string scriptArguments, string outputFilePath)
+        {
+            var targetFilePath = Path.Combine(_tempFilesDirectory, model.TargetFile.FileName);
+            await SaveStreamToDrive(targetFilePath, model.TargetFile);
+            Directory.CreateDirectory(Path.GetDirectoryName(outputFilePath)); // create output directory if doesn't exist
+
+            await RunPythonScriptAsync(_pythonPath, scriptPath, scriptArguments);
+
+            return outputFilePath;
+        }
+
+        private async Task SaveStreamToDrive(string outputPath, IFormFile file)
+        {
+            using var fileStream = new FileStream(outputPath, FileMode.Create);
+            await file.CopyToAsync(fileStream);
+        }
+
+        private async static Task RunProcessAsync(string filePath, string arguments)
+        {
+            ProcessStartInfo psi = new()
+            {
+                FileName = filePath,
+                Arguments = arguments,
                 RedirectStandardOutput = true,
                 RedirectStandardError = true,
                 UseShellExecute = false,
                 CreateNoWindow = true
             };
 
-            using (Process process = new Process { StartInfo = psi })
+            using (Process process = new()
+            { StartInfo = psi })
             {
                 var output = new List<string>();
                 var errors = new List<string>();
@@ -96,56 +99,37 @@ namespace Glosy.Services
                 process.OutputDataReceived += (sender, e) => { if (e.Data != null) output.Add(e.Data); };
                 process.ErrorDataReceived += (sender, e) => { if (e.Data != null) errors.Add(e.Data); };
 
-                process.Start();
+                try
+                {
+                    process.Start();
+                }
+                catch (Exception ex)
+                {
+                    throw new InvalidOperationException(ex.Message);
+                }
+
                 process.BeginOutputReadLine();
                 process.BeginErrorReadLine();
 
-                process.WaitForExit();
+                await process.WaitForExitAsync();
 
-                if (errors.Count > 0)
+                if (process.ExitCode != 0)
                 {
-                    throw new Exception($"Python script encountered errors:\n{string.Join("\n", errors)}");
+                    throw new OperationCanceledException($"{process.ProcessName} process exited with code {process.ExitCode}");
                 }
-
-                return string.Join("\n", output);
             }
         }
 
-        private void ConvertWebmToWav(string inputPath, string outputPath)
+        private async static Task RunPythonScriptAsync(string pythonPath, string scriptPath, string scriptArguments)
         {
-            ProcessStartInfo psi = new ProcessStartInfo
-            {
-                FileName = _ffmpegPath,
-                Arguments = $"-y -i {inputPath} {outputPath}",
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                //UseShellExecute = false,
-                //CreateNoWindow = true
-            };
+            await RunProcessAsync(pythonPath, $"{scriptPath} {scriptArguments}");
+        }
 
-            using (Process ff = new Process { StartInfo = psi })
-            {
-                var output = new List<string>();
-                var errors = new List<string>();
+        private async Task ConvertToWavAsync(string inputPath, string outputPath)
+        {
+            var arguments = $"-y -i {inputPath} {outputPath}";
 
-                ff.OutputDataReceived += (sender, e) => { if (e.Data != null) output.Add(e.Data); };
-                ff.ErrorDataReceived += (sender, e) => { if (e.Data != null) errors.Add(e.Data); };
-
-                ff.Start();
-                //ff.BeginOutputReadLine();
-                //ff.BeginErrorReadLine();
-
-                ff.WaitForExit();
-
-                foreach (var line in output)
-                {
-                    Console.WriteLine(line);
-                }
-                //if (errors.Count > 0)
-                //{
-                //    throw new Exception($"Ffmpeg encountered errors:\n{string.Join("\n", errors)}");
-                //}
-            }
+            await RunProcessAsync(_ffmpegPath, arguments);
         }
     }
 }
